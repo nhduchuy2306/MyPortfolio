3378d272106877dcbf7c50b9dfd65f4382ab493b
unknown <nguyenhuy1323614@gmail.com>
2022-02-10 20:04:12 +0700

    final two

--- app/js/script.js
+++ app/js/script.js
@@ -29,3 +29,43 @@ console.log(url);
 if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
   window.location.href = url;
 } 
+
+document.addEventListener("scroll", function() {
+    var scrollTop = document.documentElement.scrollTop;
+    console.log(scrollTop);
+    if(scrollTop >= 0 && scrollTop < 721){
+        menu[0].classList.add("active");
+        menu[1].classList.remove("active");
+        menu[2].classList.remove("active");
+        menu[3].classList.remove("active");
+        menu[4].classList.remove("active");
+    }
+    else if(scrollTop >= 721 && scrollTop < 1443){
+        menu[0].classList.remove("active");
+        menu[1].classList.add("active");
+        menu[2].classList.remove("active");
+        menu[3].classList.remove("active");
+        menu[4].classList.remove("active");
+    }
+    else if(scrollTop >= 1443 && scrollTop < 2164){
+        menu[0].classList.remove("active");
+        menu[1].classList.remove("active");
+        menu[2].classList.add("active");
+        menu[3].classList.remove("active");
+        menu[4].classList.remove("active");
+    }
+    else if(scrollTop >= 2164 && scrollTop <2886){
+        menu[0].classList.remove("active");
+        menu[1].classList.remove("active");
+        menu[2].classList.remove("active");
+        menu[3].classList.add("active");
+        menu[4].classList.remove("active");
+    }
+    else{
+        menu[0].classList.remove("active");
+        menu[1].classList.remove("active");
+        menu[2].classList.remove("active");
+        menu[3].classList.remove("active");
+        menu[4].classList.add("active");
+    }
+})
